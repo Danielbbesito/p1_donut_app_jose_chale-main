faef6ddca8fe9b6859b9978236db2c93c9d12144
Danielbbesito <chalejose94@gmail.com>
2024-09-09 13:19:36 -0600

    myTab añadido

---
+++ README.md
@@ -0,0 +1,3 @@
+# p1_donut_app_jose_chale
+
+A new Flutter project.
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:p1_donut_app_jose_chale/utils/my_tab.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> { 
+  List<Widget> myTabs = [
+    const MyTab(
+      iconPath:"lib/icons/donut.png" ,
+      ),
+    const MyTab(
+      iconPath:"lib/icons/burger.png" ,
+      ),
+    const MyTab(
+      iconPath:"lib/icons/smoothie.png" ,
+      ),
+    const MyTab(
+      iconPath:"lib/icons/pancakes.png" ,
+      ),
+    const MyTab(
+      iconPath:"lib/icons/pizza.png" ,
+      ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: const Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey,
+            ),
+          ),
+          actions: const [Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )],
+        ),
+      body: Column(
+        children: [
+       const Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text("I want to ", style: TextStyle(fontSize: 24)),
+              Text("Eat", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, decoration: TextDecoration.underline ))
+            ],
+      
+          ),
+        ),
+          TabBar(tabs: myTabs),
+        ],
+        ),
+      ),
+    );
+  }
+}
